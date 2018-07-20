626067e7d6285de07d084449b82acbd7904e06fe
mikeyrayvon <mikey@interglobal.vision>
2018-07-20 17:01:26 -0500

    scaffold exposiciones page

--- lib/thumbnail-sizes.php
+++ lib/thumbnail-sizes.php
@@ -12,4 +12,9 @@ if( function_exists( 'add_image_size' ) ) {
 
   add_image_size( 'resident-archive', 273, 381, true );
   add_image_size( 'resident-current', 430, 600, true );
+
+  add_image_size( 'exhibition-current', 1840, 750, true );
+  add_image_size( 'exhibition-archive', 430, 300, true );
+
+  add_image_size( 'event-current', 900, 628, true );
 }
