7d7bbbe09d6388c512d3fd33a34256cec6d3dd46
mikeyrayvon <mikey@interglobal.vision>
2018-07-19 22:37:01 -0500

    add resdient thumb sizes

--- lib/thumbnail-sizes.php
+++ lib/thumbnail-sizes.php
@@ -9,4 +9,7 @@ if( function_exists( 'add_image_size' ) ) {
   add_image_size( 'opengraph', 1200, 630, true );
 
   add_image_size( 'gallery', 1200, 9999, false );
+
+  add_image_size( 'resident-archive', 273, 381, true );
+  add_image_size( 'resident-current', 430, 600, true );
 }
