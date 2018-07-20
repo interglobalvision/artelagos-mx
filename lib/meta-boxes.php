fe6cdf4dcb76816878d8a60363a176833eec4579
mikeyrayvon <mikey@interglobal.vision>
2018-07-19 21:05:05 -0500

    add post types and metaboxes

--- lib/meta-boxes.php
+++ lib/meta-boxes.php
@@ -17,27 +17,75 @@ function get_post_objects( $query_args ) {
 
 
 /**
- * Include and setup custom metaboxes and fields.
- *
- * @category YourThemeOrPlugin
- * @package  Metaboxes
- * @license  http://www.opensource.org/licenses/gpl-license.php GPL v2.0 (or later)
- * @link     https://github.com/WebDevStudios/CMB2
- */
+  * Include and setup custom metaboxes and fields.
+  *
+  * @category YourThemeOrPlugin
+  * @package  Metaboxes
+  * @license  http://www.opensource.org/licenses/gpl-license.php GPL v2.0 (or later)
+  * @link     https://github.com/WebDevStudios/CMB2
+  */
 
 /**
- * Hook in and add metaboxes. Can only happen on the 'cmb2_init' hook.
- */
+  * Hook in and add metaboxes. Can only happen on the 'cmb2_init' hook.
+  */
+
 add_action( 'cmb2_init', 'igv_cmb_metaboxes' );
 function igv_cmb_metaboxes() {
 
   // Start with an underscore to hide fields from custom fields list
   $prefix = '_igv_';
 
-  /**
-   * Metaboxes declarations here
-   * Reference: https://github.com/WebDevStudios/CMB2/blob/master/example-functions.php
-   */
+  $common_metabox = new_cmb2_box( array(
+		'id'            => $prefix . 'common_metabox',
+		'title'         => esc_html__( 'Fields', 'cmb2' ),
+		'object_types'  => array( 'exhibition', 'event', 'resident' ), // Post type
+	) );
+
+  $common_metabox->add_field( array(
+		'name' => esc_html__( 'Start Date', 'cmb2' ),
+		'id'   => $prefix . 'start_date',
+		'type' => 'text_date_timestamp',
+	) );
+
+  $common_metabox->add_field( array(
+		'name' => esc_html__( 'End Date', 'cmb2' ),
+		'id'   => $prefix . 'end_date',
+		'type' => 'text_date_timestamp',
+	) );
+
+  $common_metabox->add_field( array(
+		'name'         => esc_html__( 'Documentation', 'cmb2' ),
+		'id'           => $prefix . 'documentation',
+		'type'         => 'file_list',
+		'preview_size' => array( 150, 150 ), // Default: array( 50, 50 )
+	) );
+
+  $options_metabox = new_cmb2_box( array(
+		'id'            => $prefix . 'options_metabox',
+		'title'         => esc_html__( 'Fields', 'cmb2' ),
+		'object_types'  => array( 'exhibition', 'event' ), // Post type
+	) );
+
+  $options_metabox->add_field( array(
+		'name' => esc_html__( 'Opening times', 'cmb2' ),
+		'id'   => $prefix . 'opening_times',
+		'type' => 'text',
+	) );
+
+  $options_metabox->add_field( array(
+    'name'      	=> __( 'Residents', 'cmb2' ),
+    'id'        	=> 'related_residents',
+    'type'      	=> 'post_search_ajax',
+    'desc'			=> __( '(Start typing resident name)', 'cmb2' ),
+    // Optional :
+    'limit' => 100,
+    'sortable' 	 	=> true, 	// Allow selected items to be sortable (default false)
+    'query_args'	=> array(
+      'post_type'			=> array( 'resident' ),
+      'post_status'		=> array( 'publish' ),
+      'posts_per_page'	=> -1
+    )
+  ) );
 
 }
 ?>
--- lib/post-types.php
+++ lib/post-types.php
@@ -5,8 +5,17 @@ function add_menu_icons_styles(){
 ?>
 
 <style>
-#menu-posts-project .dashicons-admin-post:before {
-    content: '\f319';
+#menu-posts-exhibition .dashicons-admin-post:before {
+  content: "\f128";
+}
+#menu-posts-event .dashicons-admin-post:before {
+  content: "\f508";
+}
+#menu-posts-resident .dashicons-admin-post:before {
+  content: "\f118";
+}
+#menu-posts-studio .dashicons-admin-post:before {
+  content: "\f309";
 }
 </style>
 
@@ -16,23 +25,23 @@ add_action( 'admin_head', 'add_menu_icons_styles' );
 
 
 //Register Custom Post Types
-add_action( 'init', 'register_cpt_project' );
+add_action( 'init', 'register_cpt_exhibition' );
 
-function register_cpt_project() {
+function register_cpt_exhibition() {
 
   $labels = array(
-    'name' => _x( 'Projects', 'project' ),
-    'singular_name' => _x( 'Project', 'project' ),
-    'add_new' => _x( 'Add New', 'project' ),
-    'add_new_item' => _x( 'Add New Project', 'project' ),
-    'edit_item' => _x( 'Edit Project', 'project' ),
-    'new_item' => _x( 'New Project', 'project' ),
-    'view_item' => _x( 'View Project', 'project' ),
-    'search_items' => _x( 'Search Projects', 'project' ),
-    'not_found' => _x( 'No projects found', 'project' ),
-    'not_found_in_trash' => _x( 'No projects found in Trash', 'project' ),
-    'parent_item_colon' => _x( 'Parent Project:', 'project' ),
-    'menu_name' => _x( 'Projects', 'project' ),
+    'name' => _x( 'Exhibitions', 'exhibition' ),
+    'singular_name' => _x( 'Exhibition', 'exhibition' ),
+    'add_new' => _x( 'Add New', 'exhibition' ),
+    'add_new_item' => _x( 'Add New Exhibition', 'exhibition' ),
+    'edit_item' => _x( 'Edit Exhibition', 'exhibition' ),
+    'new_item' => _x( 'New Exhibition', 'exhibition' ),
+    'view_item' => _x( 'View Exhibition', 'exhibition' ),
+    'search_items' => _x( 'Search Exhibitions', 'exhibition' ),
+    'not_found' => _x( 'No exhibitions found', 'exhibition' ),
+    'not_found_in_trash' => _x( 'No exhibitions found in Trash', 'exhibition' ),
+    'parent_item_colon' => _x( 'Parent Exhibition:', 'exhibition' ),
+    'menu_name' => _x( 'Exhibitions', 'exhibition' ),
   );
 
   $args = array(
@@ -56,5 +65,137 @@ function register_cpt_project() {
     'capability_type' => 'post'
   );
 
-  register_post_type( 'project', $args );
+  register_post_type( 'exhibition', $args );
+}
+
+//Register Custom Post Types
+add_action( 'init', 'register_cpt_event' );
+
+function register_cpt_event() {
+
+  $labels = array(
+    'name' => _x( 'Events', 'event' ),
+    'singular_name' => _x( 'Event', 'event' ),
+    'add_new' => _x( 'Add New', 'event' ),
+    'add_new_item' => _x( 'Add New Event', 'event' ),
+    'edit_item' => _x( 'Edit Event', 'event' ),
+    'new_item' => _x( 'New Event', 'event' ),
+    'view_item' => _x( 'View Event', 'event' ),
+    'search_items' => _x( 'Search Events', 'event' ),
+    'not_found' => _x( 'No events found', 'event' ),
+    'not_found_in_trash' => _x( 'No events found in Trash', 'event' ),
+    'parent_item_colon' => _x( 'Parent Event:', 'event' ),
+    'menu_name' => _x( 'Events', 'event' ),
+  );
+
+  $args = array(
+    'labels' => $labels,
+    'hierarchical' => false,
+
+    'supports' => array( 'title', 'editor', 'thumbnail' ),
+
+    'public' => true,
+    'show_ui' => true,
+    'show_in_menu' => true,
+    'menu_position' => 5,
+
+    'show_in_nav_menus' => true,
+    'publicly_queryable' => true,
+    'exclude_from_search' => false,
+    'has_archive' => true,
+    'query_var' => true,
+    'can_export' => true,
+    'rewrite' => true,
+    'capability_type' => 'post'
+  );
+
+  register_post_type( 'event', $args );
+}
+
+//Register Custom Post Types
+add_action( 'init', 'register_cpt_resident' );
+
+function register_cpt_resident() {
+
+  $labels = array(
+    'name' => _x( 'Residents', 'resident' ),
+    'singular_name' => _x( 'Resident', 'resident' ),
+    'add_new' => _x( 'Add New', 'resident' ),
+    'add_new_item' => _x( 'Add New Resident', 'resident' ),
+    'edit_item' => _x( 'Edit Resident', 'resident' ),
+    'new_item' => _x( 'New Resident', 'resident' ),
+    'view_item' => _x( 'View Resident', 'resident' ),
+    'search_items' => _x( 'Search Residents', 'resident' ),
+    'not_found' => _x( 'No residents found', 'resident' ),
+    'not_found_in_trash' => _x( 'No residents found in Trash', 'resident' ),
+    'parent_item_colon' => _x( 'Parent Resident:', 'resident' ),
+    'menu_name' => _x( 'Residents', 'resident' ),
+  );
+
+  $args = array(
+    'labels' => $labels,
+    'hierarchical' => false,
+
+    'supports' => array( 'title', 'editor', 'thumbnail' ),
+
+    'public' => true,
+    'show_ui' => true,
+    'show_in_menu' => true,
+    'menu_position' => 5,
+
+    'show_in_nav_menus' => true,
+    'publicly_queryable' => true,
+    'exclude_from_search' => false,
+    'has_archive' => true,
+    'query_var' => true,
+    'can_export' => true,
+    'rewrite' => true,
+    'capability_type' => 'post'
+  );
+
+  register_post_type( 'resident', $args );
+}
+
+//Register Custom Post Types
+add_action( 'init', 'register_cpt_studio' );
+
+function register_cpt_studio() {
+
+  $labels = array(
+    'name' => _x( 'Studios', 'studio' ),
+    'singular_name' => _x( 'Studio', 'studio' ),
+    'add_new' => _x( 'Add New', 'studio' ),
+    'add_new_item' => _x( 'Add New Studio', 'studio' ),
+    'edit_item' => _x( 'Edit Studio', 'studio' ),
+    'new_item' => _x( 'New Studio', 'studio' ),
+    'view_item' => _x( 'View Studio', 'studio' ),
+    'search_items' => _x( 'Search Studios', 'studio' ),
+    'not_found' => _x( 'No studios found', 'studio' ),
+    'not_found_in_trash' => _x( 'No studios found in Trash', 'studio' ),
+    'parent_item_colon' => _x( 'Parent Studio:', 'studio' ),
+    'menu_name' => _x( 'Studios', 'studio' ),
+  );
+
+  $args = array(
+    'labels' => $labels,
+    'hierarchical' => false,
+
+    'supports' => array( 'title', 'editor', 'thumbnail' ),
+
+    'public' => true,
+    'show_ui' => true,
+    'show_in_menu' => true,
+    'menu_position' => 5,
+
+    'show_in_nav_menus' => true,
+    'publicly_queryable' => true,
+    'exclude_from_search' => false,
+    'has_archive' => true,
+    'query_var' => true,
+    'can_export' => true,
+    'rewrite' => true,
+    'capability_type' => 'post'
+  );
+
+  register_post_type( 'studio', $args );
 }
